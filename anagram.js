58e19dd741a6a55552ce53b6b3db844ae8296c24
SeeYou1ater <95224331+SeeYou1ater@users.noreply.github.com>
2022-04-20 12:45:14 +0300

    Delete anagram.js

--- anagram.js
+++
@@ -1,5 +0,0 @@
-var isAnagram = (test, original) =>
-    test.toLowerCase().split('').sort().join() === original.toLowerCase().split('').sort().join()
-
-
-console.log(isAnagram("Buckethead", "DeathCubeK"))
